90693dda89407d5b5fb4f95935a420797f5a9269
liupeike502-git <245433466@qq.com>
2020-11-16 03:22:20 +0800

    q

--- Base/测试下.py
+++
@@ -1,9 +0,0 @@
-# coding=UTF-8
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('http://testsaas.yunjes.cn/user/register')
-checkbox = driver.find_element_by_xpath("//*[@id='agreement-checkbox']")
-if checkbox.is_selected():
-    print('被选中')
-    checkbox.click()
